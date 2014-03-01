9af643b54eee71d334ebb8b47e81fd8d567eb472
Blake Rainwater <rainwaterblake@gmail.com>
2014-03-01 17:42:19 -0500

    Add url for making an identitone from a string

--- idt/urls.py
+++ idt/urls.py
@@ -3,5 +3,7 @@ from idt import views
 
 urlpatterns = patterns(
     '',
+    url(r'^identitone', views.get_raw_identitone, name='raw_get_identitone'),
+    url(r'^idt', views.get_raw_identitone, name='raw_get_idt'),
     url(r'^(?P<seed_hash>[0-9abcdef]{128})/', views.get_identitone, name='get_identitone'),
 )
--- idt/views.py
+++ idt/views.py
@@ -23,3 +23,29 @@ def get_identitone(request, seed_hash):
     identitone.write_identitone(seed_hash, response, time, notes, sounds, rate)
     return response
 
+def get_raw_identitone(request):
+    """
+    Generates an identitone from a seed that isn't a hash
+    """
+    response = HttpResponse(content_type='audio/wav')
+    time = 8
+    sounds = 8
+    notes = 4
+    rate = 44100
+    if 'time' in request.GET:
+        time = request.GET['time']
+    if 'sounds' in request.GET:
+        sounds = request.GET['sounds']
+    if 'notes' in request.GET:
+        notes = request.GET['notes']
+    if 'rate' in request.GET:
+        rate = request.GET['rate']
+
+    seed_string = ''
+    if 'seed' in request.GET:
+        seed_string = request.GET['seed']
+    seed = identitone.seed_from_value(seed_string)
+    seed_hash = identitone.hash_seed(seed)
+    identitone.write_identitone(seed_hash, response, time, notes, sounds, rate)
+    return response
+
